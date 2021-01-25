d567ba83f50d1c88384b735825a1245564774136
Michal Lawson <fela.miki21@gmail.com>
2021-01-24 23:50:31 -0600

    Repeated activities after class

--- 05-Third-Party-APIs/03-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
+++ 05-Third-Party-APIs/03-Algorithms/01-fizz-buzz/Unsolved/fizz-buzz.js
@@ -3,5 +3,8 @@
 // If a number is evenly divisible by 5 print "Buzz"
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
+var nums
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function(arr) {
+        for (var i=0; i <arr.length; )
+};
