4c30d75859dc1a189e66e845f85c735c5c7d87f4
Michal Lawson <fela.miki21@gmail.com>
2021-01-31 00:07:44 -0600

    04 activities 1 & 2

--- 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ 04-Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,18 @@
 // TODO: What will the following line of code log?
+// the window properties
 console.log(window);
 
 // TODO: What will the following line of code log?
+//the html properties?
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+// all the elements in the html file? or maybe the first element? so the meta data? or maybe the html tag since it's the first one?
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
+// the content of the head element which is the title which is "Window Object"?
 console.log(document.head);
+console.log(Object.keys(window));
+console.log(Object.keys(window).length);
 
